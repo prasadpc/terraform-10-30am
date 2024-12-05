bdab556ee1eb29b10a1ec7bb08747f3dea9a5c1c
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2024-12-05 11:07:20 +0530

    Update main.tf

--- day-14-input-vars/main.tf
+++ day-14-input-vars/main.tf
@@ -12,3 +12,8 @@ resource "aws_instance" "dev" {
 
 #terraform apply -var="ami=ami-0440d3b780d96b29d" -var="instance_type=t2.micro"
 #insert varaibles while apply time
+
+
+#Note: custome tfvar process ex: dev.tfvar test.tfvar ...etc    while applying time we can call required .tfvars 
+#by defualt terraform.tfvars will work
+#ex:# terraform apply -var-file="test.tfvars"
